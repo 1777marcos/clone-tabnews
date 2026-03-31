dd9b5b3a4dc1c1c1347b33e9a5a8c8bf46667167
Marcosantos <69252518+1777marcos@users.noreply.github.com>
2026-03-31 17:26:59 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home () {
-    return <h1>CoporaLuto100%!</h1>
+function Home() {
+  return <h1>CoporaLuto100%!</h1>;
+}
+function teste() {
+  console.log("teste1");
 }
 
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
