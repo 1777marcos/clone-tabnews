70784e1a8aebb933d04ded8d00c90dbe298baef0
Marcosantos <69252518+1777marcos@users.noreply.github.com>
2026-03-31 17:46:28 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
   return <h1>CoporaLuto100%!</h1>;
 }
-function teste() {
-  console.log("teste1");
-}
 
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
